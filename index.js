5e758532573f42ef956e9b2b6b1db1ffa805c89b
Sven Haider <sven.haider@web.de>
2020-07-13 13:51:40 +0200

    create skill-grid with javascript

--- index.js
+++ index.js
@@ -1,46 +1,81 @@
- // Navbar Scroll-Effect
- const navbar = document.querySelector('#navbar-wrapper');
- window.onscroll = () => { 
-   if (document.documentElement.scrollTop >= 50 ) {
-     navbar.style.background = "black";
-     navbar.style.transition = '0.8s ease'
-   } 
-   else {
-     navbar.style.background = "transparent";
-   }
- };
+// Navbar Scroll-Effect
+const navbar = document.querySelector('#navbar-wrapper');
+window.onscroll = () => { 
+  if (document.documentElement.scrollTop >= 50 ) {
+    navbar.style.background = "black";
+    navbar.style.transition = '0.8s ease'
+  } 
+  else {
+    navbar.style.background = "transparent";
+  }
+};
 
- // Automated update Month and Year in footer
- const update = () => {
-   const newDate = new Date();
-   const year = newDate.getFullYear();
-   const month = newDate.getMonth() + 1;
-   const months = ['January', 'February', 'March',
-                   'April', 'May', 'June', 'July',
-                   'August', 'September', 'October',
-                   'November', 'December'
-                 ];
-  
-   for (let i = 0; i < months.length; i++) {
-     if ((i + 1) === month) {
-       document.querySelector('.footer-copyright').innerText = `Last update ${months[i]} ${year}`;
-       break;
-     };
-   };
- };
+// Automated update Month and Year in footer
+const update = () => {
+  const newDate = new Date();
+  const year = newDate.getFullYear();
+  const month = newDate.getMonth() + 1;
+  const months = ['January', 'February', 'March',
+                  'April', 'May', 'June', 'July',
+                  'August', 'September', 'October',
+                  'November', 'December'
+                ];
 
- // mobile navbar
- const navSlide = () => {
-   const burger = document.querySelector('.burger');
-   const nav = document.querySelector('.navbar-list');
-   const navbar = document.querySelector('#navbar-wrapper');
+  for (let i = 0; i < months.length; i++) {
+    if ((i + 1) === month) {
+      document.querySelector('.footer-copyright').innerText = `Last update ${months[i]} ${year}`;
+      break;
+    };
+  };
+};
 
-   burger.addEventListener('click', () => {
-     nav.classList.toggle('nav-active');
-     navbar.style.background = "black";
-     burger.classList.toggle('toggle');
-   });
- };
+// mobile navbar
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.navbar-list');
+  const navbar = document.querySelector('#navbar-wrapper');
 
- navSlide();
- update();
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    navbar.style.background = "black";
+    burger.classList.toggle('toggle');
+  });
+};
+
+// create skill-grid
+const skillBox = document.querySelector('.skill-grid')
+const skills = [{'name':'Ruby on Rails', 'icon':'<i class="fas fa-subway"></i>'},
+              {'name':'HTML', 'icon':'<i class="fab fa-html5"></i>'},
+              {'name':'CSS', 'icon':'<i class="fab fa-css3"></i>'},
+              {'name':'JavaScript', 'icon':'<i class="fab fa-js-square"></i>'},
+              {'name':'Ruby', 'icon':'<i class="fas fa-gem"></i>'},
+              {'name':'GitHub', 'icon':'<i class="fab fa-github"></i>'},
+              {'name':'SQLite & PostgreSQL', 'icon':'<i class="fas fa-database"></i>'},
+              {'name':'Web Scraping<br>API<br>JSON', 'icon':'<i class="fas fa-globe"></i>'},
+              {'name':'Bootstrap', 'icon':'<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bootstrap-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.002 0a4 4 0 0 0-4 4v8a4 4 0 0 0 4 4h8a4 4 0 0 0 4-4V4a4 4 0 0 0-4-4h-8zm1.06 12h3.475c1.804 0 2.888-.908 2.888-2.396 0-1.102-.761-1.916-1.904-2.034v-.1c.832-.14 1.482-.93 1.482-1.816 0-1.3-.955-2.11-2.542-2.11H5.062V12zm1.313-4.875V4.658h1.78c.973 0 1.542.457 1.542 1.237 0 .802-.604 1.23-1.764 1.23H6.375zm0 3.762h1.898c1.184 0 1.81-.48 1.81-1.377 0-.885-.65-1.348-1.886-1.348H6.375v2.725z"/></svg>'},
+              {'name':'UI Design', 'icon':'<i class="fab fa-figma"></i>'},
+              {'name':'Test Driven Development', 'icon':'<i class="fas fa-code"></i>'},
+              {'name':'German<br>English<br>Spanish', 'icon':'<i class="fas fa-language"></i>'}
+            ] 
+
+const insertSkills = () => {
+  skills.forEach((skill) => {
+    const mainBox = 
+    `<div class="main-box">
+      <div class="skill-box">
+        <div class="skill">
+          ${skill.icon}
+        </div>
+        <div class="skill-back">
+          <p>${skill.name}</p> 
+        </div>
+      </div>
+    </div>`
+    skillBox.insertAdjacentHTML('beforeend', mainBox)
+  });
+};
+
+
+navSlide();
+update();
+insertSkills();
